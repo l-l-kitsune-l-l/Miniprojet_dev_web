d5ece000b4cb97acde69002f52c918bbe3a14753
Kitsu <tekohuemmanuel@gmail.com>
2026-04-04 23:17:25 +0200

    création du systeme login

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_product_index');
+        }
+
+        // Récupérer l'erreur de login s'il y en a une
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Dernier email saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+}
