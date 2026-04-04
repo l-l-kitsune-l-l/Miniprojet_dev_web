54d3aecdca8450aa8c8f5aa1b6bd74c88e4cb0a2
Kitsu <tekohuemmanuel@gmail.com>
2026-04-04 21:04:14 +0200

    adaptation du controller de product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,28 +12,37 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/product')]
-final class ProductController extends AbstractController
+class ProductController extends AbstractController
 {
-    #[Route(name: 'app_product_index', methods: ['GET'])]
+    // ===== LISTE =====
+    #[Route('/', name: 'app_product_index', methods: ['GET'])]
     public function index(ProductRepository $productRepository): Response
     {
         return $this->render('product/index.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $productRepository->findBy(
+                ['active' => true],
+                ['createdAt' => 'DESC']
+            ),
         ]);
     }
 
+    // ===== CRÉATION =====
     #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($product);
-            $entityManager->flush();
+            // Associer le vendeur connecté (activer quand l'auth sera en place)
+            // $product->setSeller($this->getUser());
 
-            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Produit créé avec succès !');
+            return $this->redirectToRoute('app_product_index');
         }
 
         return $this->render('product/new.html.twig', [
@@ -42,6 +51,7 @@ final class ProductController extends AbstractController
         ]);
     }
 
+    // ===== DÉTAIL =====
     #[Route('/{id}', name: 'app_product_show', methods: ['GET'])]
     public function show(Product $product): Response
     {
@@ -50,16 +60,18 @@ final class ProductController extends AbstractController
         ]);
     }
 
+    // ===== ÉDITION =====
     #[Route('/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Product $product, EntityManagerInterface $em): Response
     {
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            $em->flush();
 
-            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', 'Produit mis à jour !');
+            return $this->redirectToRoute('app_product_show', ['id' => $product->getId()]);
         }
 
         return $this->render('product/edit.html.twig', [
@@ -68,14 +80,16 @@ final class ProductController extends AbstractController
         ]);
     }
 
+    // ===== SUPPRESSION =====
     #[Route('/{id}', name: 'app_product_delete', methods: ['POST'])]
-    public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, Product $product, EntityManagerInterface $em): Response
     {
         if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($product);
-            $entityManager->flush();
+            $em->remove($product);
+            $em->flush();
+            $this->addFlash('warning', 'Produit supprimé.');
         }
 
-        return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_product_index');
     }
 }
