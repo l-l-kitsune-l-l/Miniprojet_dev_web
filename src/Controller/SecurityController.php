c4337fc9af82dbb322334461be431391cef2399f
Kitsu <tekohuemmanuel@gmail.com>
2026-04-05 17:22:29 +0200

    configuration du logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ class SecurityController extends AbstractController
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout(): never
     {
-        // Symfony intercepte cette route automatiquement
+        
         throw new \Exception('This should never be reached!');
     }
 }
