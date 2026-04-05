ddb43694937e4203d792c3cb2c2de72032e9de0a
Kitsu <tekohuemmanuel@gmail.com>
2026-04-05 17:19:59 +0200

    correction

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,32 +17,38 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
-                'constraints' => [
-                    new IsTrue(
-                        message: 'You should agree to our terms.',
-                    ),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank(
-                        message: 'Please enter a password',
-                    ),
-                    new Length(
-                        min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        max: 4096,
-                    ),
-                ],
-            ])
+    ->add('lastName', TextType::class, [
+        'label' => 'Nom',
+    ])
+    ->add('firstName', TextType::class, [
+        'label' => 'Prénom',
+    ])
+    ->add('pseudo', TextType::class, [
+        'label' => 'Pseudo',
+    ])
+    ->add('email')
+    ->add('agreeTerms', CheckboxType::class, [
+        'mapped' => false,
+        'constraints' => [
+            new IsTrue(
+                message: 'You should agree to our terms.',
+            ),
+        ],
+    ])
+    ->add('plainPassword', PasswordType::class, [
+        'mapped' => false,
+        'attr' => ['autocomplete' => 'new-password'],
+        'constraints' => [
+            new NotBlank(
+                message: 'Please enter a password',
+            ),
+            new Length(
+                min: 6,
+                minMessage: 'Your password should be at least {{ limit }} characters',
+                max: 4096,
+            ),
+        ],
+    ])
         ;
     }
 
