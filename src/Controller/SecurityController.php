b9b774b85c042a662efb8da38a4e6d0f09c757f3
Kitsu <tekohuemmanuel@gmail.com>
2026-04-05 17:01:35 +0200

    adaptation du controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,16 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class SecurityController extends AbstractController
+class SecurityController extends AbstractController
 {
-    #[Route('/security', name: 'app_security')]
-    public function index(): Response
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+        // Symfony intercepte cette route automatiquement
+        throw new \Exception('This should never be reached!');
     }
 }
