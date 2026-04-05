19cf49bb3aea60338ced9f52c9a9aa4192579405
Kitsu <tekohuemmanuel@gmail.com>
2026-04-05 15:07:41 +0200

    Création des vues profiles

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo'
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('theme', ChoiceType::class, [
+                'label' => 'Thème',
+                'choices' => [
+                    'Clair' => 'light',
+                    'Sombre' => 'dark',
+                ]
+            ])
+            ->add('locale', ChoiceType::class, [
+                'label' => 'Langue',
+                'choices' => [
+                    'Français' => 'fr',
+                    'English' => 'en',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
