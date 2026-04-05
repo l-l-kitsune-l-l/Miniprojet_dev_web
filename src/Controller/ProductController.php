12b51834478d44ab7b2e04474a46865decd35d18
Kitsu <tekohuemmanuel@gmail.com>
2026-04-06 01:35:11 +0200

    correctif de la vu détails d'un produit et du controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Order;
+use App\Entity\OrderLine;
 
 #[Route('/product')]
 class ProductController extends AbstractController
