ca31e9f8f2013ca4af4436a7ed52a2b890b83c75
Kitsu <tekohuemmanuel@gmail.com>
2026-04-06 22:41:33 +0200

    ajuster les vues

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,51 +5,64 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-    ->add('lastName', TextType::class, [
-        'label' => 'Nom',
-    ])
-    ->add('firstName', TextType::class, [
-        'label' => 'Prénom',
-    ])
-    ->add('pseudo', TextType::class, [
-        'label' => 'Pseudo',
-    ])
-    ->add('email')
-    ->add('agreeTerms', CheckboxType::class, [
-        'mapped' => false,
-        'constraints' => [
-            new IsTrue(
-                message: 'You should agree to our terms.',
-            ),
-        ],
-    ])
-    ->add('plainPassword', PasswordType::class, [
-        'mapped' => false,
-        'attr' => ['autocomplete' => 'new-password'],
-        'constraints' => [
-            new NotBlank(
-                message: 'Please enter a password',
-            ),
-            new Length(
-                min: 6,
-                minMessage: 'Your password should be at least {{ limit }} characters',
-                max: 4096,
-            ),
-        ],
-    ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+                'attr' => ['placeholder' => 'Ex: otaku_fan']
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['placeholder' => 'Ex: Naruto']
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Ex: Uzumaki']
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'attr' => ['placeholder' => 'Ex: naruto@konoha.jp']
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => ['placeholder' => 'Minimum 6 caractères']
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => ['placeholder' => 'Retapez le mot de passe']
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer un mot de passe']),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Le mot de passe doit faire au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'J\'accepte les conditions d\'utilisation',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue(['message' => 'Vous devez accepter les conditions.']),
+                ],
+            ])
         ;
     }
 
